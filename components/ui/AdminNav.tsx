2267860b7b0699266a43ea6d12d870b882796ebf
Herivasm <herihernandez6@gmail.com>
2025-11-12 13:27:22 -0700

    feat: add AdminNav component for admin navigation links

---
+++ components/ui/AdminNav.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export default function AdminNav() {
+    return (
+        <header className="px-10 py-5 bg-gray-700 flex justify-between">
+            <div className="flex gap-2 items-center">
+                <Link
+                    href={'/admin/products'}
+                    className="rounded text-white font-bold p-2"
+                >Productos</Link>
+
+                <Link
+                    href={'/admin/sales'}
+                    className="rounded text-white font-bold p-2"
+                >Ventas</Link>
+
+                <Link
+                    href={'/admin/categories'}
+                    className="rounded text-white font-bold p-2"
+                >Categorías</Link>
+
+                <Link
+                    href={'/admin/suppliers'}
+                    className="rounded text-white font-bold p-2"
+                >Proveedores</Link>
+
+                <Link
+                    href={'/'}
+                    className="rounded bg-blue-400 font-bold py-2 px-10"
+                >Tienda</Link>
+            </div>
+        </header>
+    )
+}
