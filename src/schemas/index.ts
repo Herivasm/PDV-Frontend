0d39573e24b3a8a45929f909cbf6a691e1ae1084
Herivasm <herihernandez6@gmail.com>
2025-11-12 13:26:20 -0700

    feat: add schemas for Product, Category, Supplier, and related API responses

---
+++ src/schemas/index.ts
@@ -0,0 +1,127 @@
+import z from "zod";
+
+export const ProductSchema = z.object({
+    id: z.string(),
+    name: z.string(),
+    image: z.string(),
+    price: z.coerce.number(),
+    inventory: z.number(),
+    categoryId: z.number(),
+    supplierId: z.string()
+})
+export type Product = z.infer<typeof ProductSchema>
+
+export const ProductsApiResponseSchema = z.object({
+    data: z.array(ProductSchema),
+    meta: z.object({
+        totalPages: z.number(), 
+        page: z.number(),       
+        lastPage: z.number(),  
+    })
+});
+
+export const CategorySchema = z.object({
+    id: z.number(),
+    name: z.string()
+})
+export const CategoriesResponseSchema = z.array(CategorySchema)
+export const CategoriesApiResponseSchema = z.object({
+    data: z.array(CategorySchema)
+});
+
+export type Category = z.infer<typeof CategorySchema>
+
+export const CategoryFormSchema = z.object({
+    name: z.string()
+        .min(1, { message: 'El nombre de la categoría no puede ir vacío' })
+})
+
+export const SupplierSchema = z.object({
+    id: z.string(),
+    name: z.string(),
+    contact: z.string()
+})
+export const SuppliersResponseSchema = z.array(SupplierSchema)
+export const SuppliersApiResponseSchema = z.object({
+    data: z.array(SupplierSchema)
+});
+
+export type Supplier = z.infer<typeof SupplierSchema>
+
+export const SupplierFormSchema = z.object({
+    name: z.string()
+        .min(1, { message: 'El nombre del proveedor no puede ir vacío' }),
+    contact: z.string()
+        .min(1, { message: 'La información de contacto no puede ir vacía' })
+})
+
+export const ProductsResponseSchema = z.object({
+    products: z.array(ProductSchema),
+    total: z.number()
+})
+
+export const CategoryWithProductsResponseSchema = CategorySchema.extend({
+    products: z.array(ProductSchema)
+});
+
+const ShoppingCartContentsSchema = ProductSchema.pick({
+    name: true,
+    image: true,
+    price: true,
+    inventory: true,
+}).extend({
+    productId: z.string(),
+    quantity: z.number()
+})
+
+export const ShoppingCartSchema = z.array(ShoppingCartContentsSchema)
+export type ShoppingCart = z.infer<typeof ShoppingCartSchema>
+export type CartItem = z.infer<typeof ShoppingCartContentsSchema>
+
+const OrderContentSchema = z.object({
+    productId: z.string(),
+    quantity: z.number(),
+    price: z.number()
+})
+export const OrderSchema = z.object({
+    total: z.number(),
+    contents: z.array(OrderContentSchema).min(1, { message: 'El carrito no puede ir vacio' })
+})
+
+export const SuccessResponseSchema = z.object({
+    message: z.string()
+})
+export const ErrorResponseSchema = z.object({
+    message: z.array(z.string()),
+    error: z.string(),
+    statusCode: z.number()
+})
+
+export const ContentsSchema = z.object({
+    id: z.string(),
+    quantity: z.number(),
+    price: z.string(),
+    product: ProductSchema
+})
+
+export const SaleResponseSchema = z.object({
+    id: z.string(),
+    total: z.string(),
+    transactionDate: z.string(),
+    contents: z.array(ContentsSchema)
+})
+export const SalesResponseSchema = z.array(SaleResponseSchema)
+export type Sale = z.infer<typeof SaleResponseSchema>
+
+export const ProductFormSchema = z.object({
+    name: z.string()
+        .min(1, { message: 'El nombre del producto no puede ir vacío' }),
+    price: z.coerce.number({ message: 'Precio no válido' })
+        .min(1, { message: 'El precio debe ser mayor a 0' }),
+    image: z.string({ message: 'La imagen es obligatoria' }),
+    inventory: z.coerce.number({ message: 'Inventario no válido' })
+        .min(1, { message: 'El inventario debe ser mayor a 0' }),
+    categoryId: z.coerce.number({ message: 'La categoría no es válida' }),
+    supplierId: z.string()
+        .min(1, { message: 'El proveedor no puede ir vacío' })
+})
