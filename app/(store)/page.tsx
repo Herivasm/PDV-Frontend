691875d613115e0f52b6c789a815d5ce3e48f28b
Herivasm <herihernandez6@gmail.com>
2025-11-21 09:51:20 -0700

    feat: enhance product fetching with search functionality and improve StorePage layout

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,12 +1,18 @@
 import ProductCard from "@/components/products/ProductCard"
+import StoreSearch from "@/components/products/StoreSearch";
 import { ProductsApiResponseSchema } from "@/src/schemas"
 
-async function getProducts(categoryId?: string) {
+async function getProducts(categoryId?: string, search?: string) {
     const params = new URLSearchParams();
 
     params.set('limit', '30');
+
     if (categoryId) {
         params.set('category', categoryId);
+    }
+
+    if (search) {
+        params.set('search', search);
     }
 
     const url = `${process.env.API_URL}/products?${params.toString()}`
@@ -18,7 +24,7 @@ async function getProducts(categoryId?: string) {
     })
 
     if (!req.ok) {
-        throw new Error('No se pudieron cargar los productos');
+        return []
     }
 
     const json = await req.json()
@@ -27,21 +33,30 @@ async function getProducts(categoryId?: string) {
     return response.data
 }
 
-type SearchParams = Promise<{ category?: string }>
+type SearchParams = Promise<{ category?: string, search?: string }>
 
 export default async function StorePage({ searchParams }: { searchParams: SearchParams }) {
 
-    const {category: categoryId} = await searchParams
-    const products = await getProducts(categoryId)
+    const { category: categoryId, search } = await searchParams
+    const products = await getProducts(categoryId, search)
 
     return (
-        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
-            {products.map(product => (
-                <ProductCard
-                    key={product.id}
-                    product={product}
-                />
-            ))}
+        <div className="px-5">
+            <StoreSearch />
+
+            <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
+                {products.length > 0 ? (
+
+                    products.map(product => (
+                        <ProductCard
+                            key={product.id}
+                            product={product}
+                        />
+                    ))
+                ) : (
+                    <p className="text-center col-span-full">No se encontraron productos.</p>
+                )}
+            </div>
         </div>
     )
 }
